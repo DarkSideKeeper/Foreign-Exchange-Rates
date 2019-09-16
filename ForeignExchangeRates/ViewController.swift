2e2f436a2f6c7c4e7b0021522c121fd702ada6fc
Titipan Sakunwongsalee <>
2019-09-16 10:19:43 +0700

    -init project from Xcode

---
+++ ForeignExchangeRates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForeignExchangeRates
+//
+//  Created by Titipan Sakunwongsalee on 16/9/2562 BE.
+//  Copyright © 2562 Titipan Sakunwongsalee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
