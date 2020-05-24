77db8ae621913ccac50a3df9664d2266c81fe79a
Titipan Sakunwongsalee <>
2020-05-24 23:39:44 +0700

    WIP - add UI Tests

--- ForeignExchangeRatesUITests/ForeignExchangeRatesUITests.swift
+++ ForeignExchangeRatesUITests/ForeignExchangeRatesUITests.swift
@@ -7,17 +7,20 @@
 //
 
 import XCTest
+@testable import ForeignExchangeRates
 
 class ForeignExchangeRatesUITests: XCTestCase {
 
+    let app = XCUIApplication()
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+        continueAfterFailure = true
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -26,9 +29,17 @@ class ForeignExchangeRatesUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_ChangeLanguageTh() throws {
+        let navigationBar = app.navigationBars["อัตราแลกเปลี่ยน"].otherElements.firstMatch
+        if navigationBar.exists {
+            XCTAssertEqual(navigationBar.label, "อัตราแลกเปลี่ยน")
+        }
     }
-
+    
+    func test_ChangeLanguageEn() throws {
+        let navigationBar = app.navigationBars["Exchange-Rates"].otherElements.firstMatch
+        if navigationBar.exists {
+            XCTAssertEqual(navigationBar.label, "Exchange-Rates")
+        }
+    }
 }
